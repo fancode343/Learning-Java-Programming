8a509e4336edfb3d6ed7b938a93debc6eb17c998
fancode343 <118608942+fancode343@users.noreply.github.com>
2026-05-05 15:04:16 +0000

    Added Random Nuimber Algorithm

---
+++ RandomAlgorithm/Random.java
@@ -0,0 +1,24 @@
+package RandomAlgorithm;
+
+import java.util.*;
+
+public class Random {
+    public static void main(String[] Args) {
+        Scanner butang = new Scanner(System.in);
+        int random = RandomNumber.randomNum(1, 10);
+        System.out.print("Guest the number between 1 to 10: ");
+        int guest = butang.nextInt();
+        if (guest == random) {
+            System.out.println("Congratulations, You Guessed The Number");
+        } else {
+            System.out.println("Wrong! The Number is " + random);
+        }
+        int a = 0;
+        System.out.println("Random Numbers: ");
+        while (a < 10) {
+            System.out.print(RandomNumber.randomNum(1, 10) + ", ");
+            a++;
+        }
+        System.out.println();
+    }
+}
