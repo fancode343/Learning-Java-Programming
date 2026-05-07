5ac13a496fbb4c67474f05c88b861c01d0c11c8b
fancode343 <118608942+fancode343@users.noreply.github.com>
2026-05-07 12:47:48 +0000

    Edit EPS

--- OOP-Programing/EPS/FullTimeEmpoyee.java
+++ OOP-Programing/EPS/FullTimeEmpoyee.java
@@ -1,48 +1,55 @@
 package EPS;
 
-public class FullTimeEmpoyee extends Employee{
+public class FullTimeEmpoyee extends Employee {
     private int overtimeHours;
     private String performance;
-    public FullTimeEmpoyee(String name, double baseSalary, int overtimeHours, String performance){
+
+    public FullTimeEmpoyee(String name, double baseSalary, int overtimeHours, String performance) {
         super(name, baseSalary);
         this.overtimeHours = overtimeHours;
         this.performance = performance;
     }
-    public int getOverTimeHours(){
+
+    public int getOverTimeHours() {
         return overtimeHours;
     }
-    public void setOverTimeHours(int overtimeHours){
+
+    public void setOverTimeHours(int overtimeHours) {
         this.overtimeHours = overtimeHours;
     }
-    public String getPerformance(){
+
+    public String getPerformance() {
         return performance;
     }
-    public void setPerformance(String performance){
+
+    public void setPerformance(String performance) {
         this.performance = performance;
     }
-    double bonusRate(){
-        if(performance.equals("Excellent")){
+
+    double bonusRate() {
+        if (performance.equals("Excellent")) {
             return .1;
-        } else if(performance.equals("Good")) {
+        } else if (performance.equals("Good")) {
             return 0.05;
-        } else if(performance.equals("Average")) {
+        } else if (performance.equals("Average")) {
             return 0.02;
-        } else{
+        } else {
             return 0;
         }
     }
+
     @Override
-    public double computeSalary(){
+    public double computeSalary() {
         double baseSalary = super.computeSalary();
-        double overtimePay = overtimeHours*150;
-        
+        double overtimePay = overtimeHours * 150;
+
         double gros = baseSalary + overtimePay;
-        double bunus = gros*bonusRate();
-        double totalSalary = gros+bunus;
-        if(totalSalary <= 0){
+        double bunus = gros * bonusRate();
+        double totalSalary = gros + bunus;
+        if (totalSalary <= 0) {
             return 0;
-        }else{
+        } else {
             return totalSalary;
-        } 
+        }
     }
 }
