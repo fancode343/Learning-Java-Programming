93abcab6dc0ecaae776540c55333ea338a5af6b3
fancode343 <118608942+fancode343@users.noreply.github.com>
2026-05-09 14:22:56 +0000

    new files

--- AllInOneSystem/Main.java
+++ AllInOneSystem/Main.java
@@ -1,5 +1,9 @@
 package AllInOneSystem;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        int number = 12;
+        SO.Pln(number + "Helo");
+
+    }
 }
---
+++ AllInOneSystem/SO.java
@@ -0,0 +1,10 @@
+package AllInOneSystem;
+
+public class SO {
+    public static void P(Object ob) {
+        System.out.print(ob);
+    }
+    public static void Pln(Object ob) {
+        System.out.println(ob);
+    }
+}
