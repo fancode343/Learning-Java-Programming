a4ed72fe483a958374d768bac7a894c4e1eecb2c
fancode343 <118608942+fancode343@users.noreply.github.com>
2026-05-10 13:49:49 +0000

    New Code

--- AllInOneSystem/Main.java
+++ AllInOneSystem/Main.java
@@ -6,13 +6,22 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
         List<Class<?>> classes = PackageScanner.getClasses("AllInOneSystem.Systems");
+
+        SO.Pln("-----------ALL-IN-ONE-SYSTEM-----------");
+        Thread.sleep(1000);
+        SO.Pln("-");
+        Thread.sleep(1000);
+
 
         for (Class<?> cls : classes) {
             SO.Pln("Class: " + cls.getName());
+
+            //getname
             Method method = cls.getMethod("getName");
             String result = (String) method.invoke(null);
+
+            
             SO.Pln("Result: " + result);
             SO.Pln();
         }
--- AllInOneSystem/PackageScanner.java
+++ AllInOneSystem/PackageScanner.java
@@ -8,42 +8,29 @@ import java.util.List;
 public class PackageScanner {
 
     public static List<Class<?>> getClasses(String packageName) throws Exception {
-
         List<Class<?>> classes = new ArrayList<>();
-
         String path = packageName.replace('.', '/');
-
         ClassLoader classLoader =
                 Thread.currentThread().getContextClassLoader();
-
         URL resource = classLoader.getResource(path);
-
         if (resource == null) {
             throw new Exception("Package not found: " + packageName);
         }
-
         File directory = new File(resource.getFile());
-
         if (!directory.exists()) {
             throw new Exception("Folder not found: " + directory);
         }
-
         for (File file : directory.listFiles()) {
-
             System.out.println("Found file: " + file.getName());
-
             if (file.getName().endsWith(".class")) {
-
                 String className =
                         packageName + "." +
                         file.getName().replace(".class", "");
 
                 Class<?> cls = Class.forName(className);
-
                 classes.add(cls);
             }
         }
-
         return classes;
     }
 }
