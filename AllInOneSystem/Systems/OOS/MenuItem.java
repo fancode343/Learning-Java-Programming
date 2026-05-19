a5c068ffdc23bdd20e54828bd403b43dfc029fc7
fancode343 <118608942+fancode343@users.noreply.github.com>
2026-05-19 14:48:45 +0000

    Added MenuItem

---
+++ AllInOneSystem/Systems/OOS/MenuItem.java
@@ -0,0 +1,29 @@
+package OOS;
+
+public class MenuItem {
+    private String name;
+    private double price;
+    MenuItem(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setPrice(double price){
+        if(price <= 0) {
+            this.price = 0;
+        }else{
+            this.price = price;
+        }
+    }
+    public double getTotalPrice(){
+        return price;
+    }
+}
