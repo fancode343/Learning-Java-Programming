8edb08ecfdef2cfb6edb660f960cf95926de5d48
ShinZinXU <118608942+fancode343@users.noreply.github.com>
2026-05-26 21:29:14 +0800

    Update Main.java

--- OOP-Programing/ABSTRACT/Main.java
+++ OOP-Programing/ABSTRACT/Main.java
@@ -1,1 +1,10 @@
-added
+public class Main{
+  public static void main(Stringp[]) args){
+    Dog d = new Dog();
+    Cat c = new Cat();
+    Monkey m = new Monkey();
+    Snake s = new Snake();
+    System.out.println("Animal Sounds: ");
+    System.out.println("Dog: "+d);
+  }
+}
