1969bee5c381eadc28b64b532961e68b8d0b35db
ShinZinXU <118608942+fancode343@users.noreply.github.com>
2026-05-28 21:41:13 +0800

    Create Armstrong.java

---
+++ Basics/Armstrong.java
@@ -0,0 +1,16 @@
+//Armstrong
+//EX. Userinput = 153
+// The output is true
+// An Armstrong number is a number that equals the sum of its own digits each raised to the power of the number of digits
+// 153 = 3 digit
+// 1³ + 5³ + 8³ = 153
+
+import java.util.Scanner;
+public class Armstrong{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter number: ");
+    int userinput = input.nextInt();
+    
+  }
+}
