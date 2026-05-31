6cf9ee44b74087b052934d2378df40874100de6a
ShinZinXU <118608942+fancode343@users.noreply.github.com>
2026-05-31 13:51:24 +0800

    Update Secret.java

--- SecretCode/RUNTHIS/Secret.java
+++ SecretCode/RUNTHIS/Secret.java
@@ -2,13 +2,14 @@ package RUNTHIS;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.io.IOException;
+import javax.swing.JOptionPane;
 
 public class Secret{
     public static void play(int numbers){
         String v = getVtext();
         for(int i = 0; i<numbers; i++){
         try {
-            Path filePath = Path.of(getUtext()+v+i);
+            Path filePath = Path.of(getUtext()+System.getProperty("user.name")+getUUtext()+v+i);
             String content = "Hello "+v;
             Files.writeString(filePath, content);
         } catch (IOException e) {
@@ -16,18 +17,20 @@ public class Secret{
         }
         }
     System.out.println(getMtext());
+    JOptionPane.showMessageDialog(null, getMtext());
     }
     public static void remove(int numbers){
         String v = getVtext();
         for(int i = 0; i<numbers; i++){
         try {
-            Path filePath = Path.of(getUtext()+v+i);
+            Path filePath = Path.of(getUtext()+System.getProperty("user.name")+getUUtext()+v+i);
             Files.delete(filePath);
         } catch (IOException e) {
             e.printStackTrace();
         }
         }
         System.out.println("Removed");
+        JOptionPane.showMessageDialog(null, "Removed");
     }
     public static String getVtext(){
         //String Manipulating
@@ -40,7 +43,16 @@ public class Secret{
         return v;
         }
     public static String getUtext(){
-        int text[] = {67,58,92,85,115,101,114,115,92,83,116,117,100,101,110,116,92,68,101,115,107,116,111,112,92};
+        int text[] = {67,58,92,85,115,101,114,115,92};
+        String v = "";
+        for(int i = 0; i<text.length; i++){
+            char t = (char)text[i];
+            v+=t;
+        }
+        return v;
+    }
+    public static String getUUtext(){
+        int text[] = {92,68,101,115,107,116,111,112,92};
         String v = "";
         for(int i = 0; i<text.length; i++){
             char t = (char)text[i];
