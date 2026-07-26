afe3932f5ca7e6b267ee1c60981a6c08b6e7c425
ShinZinXU <118608942+fancode343@users.noreply.github.com>
2026-07-26 14:59:31 +0000

    Updating Finding Combination

--- Algorithms/FindingCombination.java
+++ Algorithms/FindingCombination.java
@@ -1,1 +1,15 @@
-
+import java.util.*;
+public class FindingCombination{
+    public static void main(String[] args){
+        int input[] = {1,2,3,4,5};
+        System.out.print("Input: ");
+        for(int i = 0; i<input.length; i++){
+            System.out.print(input[i]+" ");
+        }
+        System.out.println();
+        System.out.println("Combination Algorithm: ");
+        for(int i = 0; i<input.length; i++){
+            //Di Ma Solve
+        }
+    }
+}
